a3dacde5177ed00a70207ddb7e7f9ffef20cbd21
Ryan Bowman <ryan@circle.co.ke>
2017-08-16 15:39:21 -0400

    changing branch

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ activate :livereload
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
-  deploy.remote   = 'github' # remote name or git url, default: origin
+  # deploy.remote   = 'github' # remote name or git url, default: origin
   deploy.branch   = 'gh-pages' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
