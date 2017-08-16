ba4d4dcf558bff82354559d68c76ed9a9b1dea08
Ryan Bowman <ryan@circle.co.ke>
2017-08-16 15:39:39 -0400

    Automated commit at 2017-08-16 19:39:39 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,12 @@
+
+
+// This is where it all goes :)
+$( document ).ready(function() {
+    
+    $('.story-group--swipe').slick({
+      arrows:false,
+      centerMode: true,
+      centerPadding: '50px',
+      dots: true
+    });
+});
