84d4ee4e9e1a79eadc0c4b9b1a0f749a6a578278
Ryan Bowman <ryan@circle.co.ke>
2017-08-16 15:49:51 -0400

    auto build before

--- config.rb
+++ config.rb
@@ -11,10 +11,12 @@ configure :build do
 end
 
 activate :deploy do |deploy|
+  deploy.build_before = true
   deploy.deploy_method = :git
   # Optional Settings
   # deploy.remote   = 'github' # remote name or git url, default: origin
   deploy.branch   = 'gh-pages' # default: gh-pages
+
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
