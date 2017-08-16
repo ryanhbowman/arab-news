841438e88fde39c5bc07026604b214832943bd66
Ryan Bowman <ryan@circle.co.ke>
2017-08-15 23:00:34 -0400

    first commit

---
+++ source/javascripts/site.js
@@ -0,0 +1,16 @@
+
+
+// This is where it all goes :)
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.story-group--swipe').slick({
+      arrows:false,
+      centerMode: true,
+      centerPadding: '50px',
+      dots: true
+    });
+
+
+
+
+});
