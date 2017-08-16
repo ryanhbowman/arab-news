0ef7355e91bacf8b74f8878280ab49f6acb7aed8
Ryan Bowman <ryan@circle.co.ke>
2017-08-16 15:33:40 -0400

    removing app capable on header

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,15 +2,11 @@
 
 // This is where it all goes :)
 $( document ).ready(function() {
-    console.log( "ready!" );
+    
     $('.story-group--swipe').slick({
       arrows:false,
       centerMode: true,
       centerPadding: '50px',
       dots: true
     });
-
-
-
-
 });
